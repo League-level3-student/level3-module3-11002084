0b3a33d15d168c17c39966d8a76bb63d692ca9e9
11002084 <36611325+11002084@users.noreply.github.com>
2020-08-09 15:58:15 -0700

    8/9/2020
    Started working on Animal Farm

---
+++ src/_01_AnimalFarm/Cow.java
@@ -0,0 +1,16 @@
+package _01_AnimalFarm;
+
+public class Cow extends Animal{
+
+	@Override
+	public void makeNoise() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void walk() {
+		// TODO Auto-generated method stub
+		
+	}
+}
